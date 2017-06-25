577deec0a69871a352e5c6ff4794dbb02c3d29c5
Mike Breese <Michael1Breese@gmail.com>
2017-06-25 14:05:54 -0600

    invis platforms improved

--- Assets/Scripts/Level and Puzzles/InvisiblePlatform.cs
+++ Assets/Scripts/Level and Puzzles/InvisiblePlatform.cs
@@ -3,50 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InvisiblePlatform : MonoBehaviour {
-    [SerializeField]
-    private float physicalTouchingColor = 255f;
-    [SerializeField]
-    private float physicalCloseColor = 128f;
 
+    public bool fadingIn;
+    private float fadeInSpeed = 1f;
+    private float fadeAlpha;
     [SerializeField]
-    private float wispTouchingColor = 128f;
-    [SerializeField]
-    private float wispCloseColor = 60f;
+    private float fadeInCap;
 
-    private bool wispClose;
-    private bool wispTouching;
-    private bool playerOrEnemyClose;
-    private bool playerOrEnemyTouching;
-    //WispClose
-    //WispTouching
-    //PlayerOrEnemyClose
-    //PlayerOrEnemyTouching
 
     // Use this for initialization
     void Start () {
         //Maybe change from flipping active/inactive to an opacity depending on how close the player is.
 
+        if (gameObject.tag == "Ground")
+        {
+            fadeInCap = 10f;
+        }
+        else
+        {
+            fadeInCap = 1f;
+        }
         this.GetComponent<SpriteRenderer>().material.color = new Color(this.GetComponent<SpriteRenderer>().material.color.r, this.GetComponent<SpriteRenderer>().material.color.g, this.GetComponent<SpriteRenderer>().material.color.b, 0f);
 	}
 
-    void OnTriggerEnter2D(Collider2D other)
+    void Update()
+    {
+        if (fadingIn)
+        {
+            if (fadeAlpha < fadeInCap)
+            {
+                fadeAlpha = fadeAlpha + (fadeInSpeed) * Time.deltaTime;
+                Debug.Log("Fade Alpha: " + fadeAlpha);
+            }
+
+            this.GetComponent<SpriteRenderer>().material.color = new Color(this.GetComponent<SpriteRenderer>().material.color.r, this.GetComponent<SpriteRenderer>().material.color.g, this.GetComponent<SpriteRenderer>().material.color.b, fadeAlpha);
+            if (fadeAlpha >= fadeInCap)
+            {
+                fadeAlpha = fadeInCap;
+            }
+        }
+        else
+        {
+            if (fadeAlpha > 0)
+            {
+                fadeAlpha = fadeAlpha - fadeInSpeed * Time.deltaTime;
+            }
+
+            this.GetComponent<SpriteRenderer>().material.color = new Color(this.GetComponent<SpriteRenderer>().material.color.r, this.GetComponent<SpriteRenderer>().material.color.g, this.GetComponent<SpriteRenderer>().material.color.b, fadeAlpha);
+
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
     {
         if (gameObject.CompareTag("Ground"))
         {
             if (other.CompareTag("Player") || other.CompareTag("Enemy"))
             {
-                //sR.enabled = true;
-                Debug.Log(other + " Is close to it, but not touching it");
-                this.GetComponent<SpriteRenderer>().material.color = new Color(this.GetComponent<SpriteRenderer>().material.color.r, this.GetComponent<SpriteRenderer>().material.color.g, this.GetComponent<SpriteRenderer>().material.color.b, physicalCloseColor);
+                //Debug.Log(other + " Is close to it, but not touching it");
+                if (!fadingIn)
+                fadingIn = true;
             }
         }
-        else if(gameObject.CompareTag("Wisp Stopper"))
+        else if (gameObject.CompareTag("Wisp Stopper"))
         {
             if (other.CompareTag("Wisp"))
             {
-                //sR.enabled = true;
-                Debug.Log(other + " Is close to it, but not touching it");
-                this.GetComponent<SpriteRenderer>().material.color = new Color(this.GetComponent<SpriteRenderer>().material.color.r, this.GetComponent<SpriteRenderer>().material.color.g, this.GetComponent<SpriteRenderer>().material.color.b, wispCloseColor);
+                //Debug.Log(other + " Is close to it, but not touching it");
+                if(!fadingIn)
+                fadingIn = true;
             }
         }
     }
@@ -59,8 +84,8 @@ public class InvisiblePlatform : MonoBehaviour {
             {
                 //sR.enabled = false;
                 Debug.Log(other + " Is NO LONGER CLOSE to it, but not touching it");
-                this.GetComponent<SpriteRenderer>().material.color = new Color(this.GetComponent<SpriteRenderer>().material.color.r, this.GetComponent<SpriteRenderer>().material.color.g, this.GetComponent<SpriteRenderer>().material.color.b, 0f);
-            } 
+                fadingIn = false;
+            }
         }
         else if (gameObject.CompareTag("Wisp Stopper"))
         {
@@ -68,65 +93,8 @@ public class InvisiblePlatform : MonoBehaviour {
             {
                 //sR.enabled = false;
                 Debug.Log(other + " Is NO LONGER CLOSE to it, but not touching it");
-                this.GetComponent<SpriteRenderer>().material.color = new Color(this.GetComponent<SpriteRenderer>().material.color.r, this.GetComponent<SpriteRenderer>().material.color.g, this.GetComponent<SpriteRenderer>().material.color.b, 0f);
+                fadingIn = false;
             }
-        }
-    }
-
-    void OnCollisionStay2D(Collision2D other)
-    {
-        if (gameObject.CompareTag("Ground"))
-        {
-            if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Enemy"))
-            {
-                //sR.enabled = true;
-                Debug.Log(other + " Is TOUCHING it");
-                this.GetComponent<SpriteRenderer>().material.color = new Color(this.GetComponent<SpriteRenderer>().material.color.r, this.GetComponent<SpriteRenderer>().material.color.g, this.GetComponent<SpriteRenderer>().material.color.b, physicalTouchingColor);
-            }
-        }
-        else if (gameObject.CompareTag("Wisp Stopper"))
-        {
-            if (other.gameObject.CompareTag("Wisp"))
-            {
-                //sR.enabled = true;
-                Debug.Log(other + " Is TOUCHING it");
-                this.GetComponent<SpriteRenderer>().material.color = new Color(this.GetComponent<SpriteRenderer>().material.color.r, this.GetComponent<SpriteRenderer>().material.color.g, this.GetComponent<SpriteRenderer>().material.color.b, wispTouchingColor);
-            }
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D other)
-    {
-        if (gameObject.CompareTag("Ground"))
-        {
-            if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Enemy"))
-            {
-                //sR.enabled = false;
-                Debug.Log(other + " Is NO LONGER TOUCHING it, but not touching it");
-                this.GetComponent<SpriteRenderer>().material.color = new Color(this.GetComponent<SpriteRenderer>().material.color.r, this.GetComponent<SpriteRenderer>().material.color.g, this.GetComponent<SpriteRenderer>().material.color.b, 0f);
-            }
-        }
-        else if (gameObject.CompareTag("Wisp Stopper"))
-        {
-            if (other.gameObject.CompareTag("Wisp"))
-            {
-                //sR.enabled = false;
-                Debug.Log(other + " Is NO LONGER TOUCHING it, but not touching it");
-                this.GetComponent<SpriteRenderer>().material.color = new Color(this.GetComponent<SpriteRenderer>().material.color.r, this.GetComponent<SpriteRenderer>().material.color.g, this.GetComponent<SpriteRenderer>().material.color.b, 0f);
-            }
-        }
-    }
-
-    void PlatformDisplay()
-    {
-        //WispClose
-        //WispTouching
-        //PlayerOrEnemyClose
-        //PlayerOrEnemyTouching
-
-        if (wispClose)
-        {
-            this.GetComponent<SpriteRenderer>().material.color = new Color(this.GetComponent<SpriteRenderer>().material.color.r, this.GetComponent<SpriteRenderer>().material.color.g, this.GetComponent<SpriteRenderer>().material.color.b, wispCloseColor);
         }
     }
 }
