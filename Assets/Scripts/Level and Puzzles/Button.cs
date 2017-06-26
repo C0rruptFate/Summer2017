cafd2131d92df6132697cefbede44e0ede84fbe0
Mike Breese <Michael1Breese@gmail.com>
2017-06-26 15:47:01 -0600

    working on pressure plates

---
+++ Assets/Scripts/Level and Puzzles/Button.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    [HideInInspector]
+    public bool pushed = false;
+
+    [HideInInspector]
+    private GameObject pressurePlateMaster;
+
+    [HideInInspector]
+    public bool iShouldBeActiveBool;
+
+    // Use this for initialization
+    void Start () {
+        pressurePlateMaster = transform.parent.gameObject;
+
+        if (iShouldBeActiveBool)
+        gameObject.SetActive(true);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if ((other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Enemy")) && !pushed)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.25f, transform.position.z);
+            pushed = true;
+            pressurePlateMaster.GetComponent<PressurePlate>().CheckandIncreaseButtons();
+            Debug.Log("Button pushed");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //Checks to see if a player or enemy is touching it. And if their is more than 1 player in the game
+        if ((other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Enemy")) && pressurePlateMaster.GetComponent<PressurePlate>().holdDownButtons)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.25f, transform.position.z);
+            pushed = false;
+            pressurePlateMaster.GetComponent<PressurePlate>().CheckandDecreaseButtons();
+            Debug.Log("Button released");
+        }
+    }
+
+    public void IShouldBeActive()
+    {
+        Debug.Log("Button is now active");
+        gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Level and Puzzles/PressurePlate.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour {
+
+    public bool holdDownButtons = true;
+
+    [HideInInspector]
+    public int pressurePlatePushedCount;
+
+    private int pressurePlateTotal;
+
+    //private Transform Door;
+
+    public GameObject[] myButtons;
+    private int myButtonCount;
+
+    private GameObject gm;
+    // Use this for initialization
+    void Start () {
+        //Makes sure that buttons only need to be held down if there are 2 or more players in the game.
+        gm = GameObject.Find("Game Manager");
+        if (gm.GetComponent<GameController>().totalPlayerCount <= 1)
+            holdDownButtons = false;
+
+        foreach (Transform child in transform)
+        {
+            if (child.GetComponent<Button>() != null && child.gameObject.activeSelf)
+            {
+                //myButtons.Add(child.gameObject);
+                pressurePlateTotal++;
+            }
+        }
+
+        if (pressurePlateTotal >= gm.GetComponent<GameController>().totalPlayerCount)
+        {
+            int targetButton = pressurePlateTotal - 1;
+            for (int i = pressurePlateTotal; i >= gm.GetComponent<GameController>().totalPlayerCount; i--)
+            {
+                myButtons[targetButton].GetComponent<Button>().IShouldBeActive();
+                myButtons[targetButton].GetComponent<Button>().iShouldBeActiveBool = true;
+                pressurePlateTotal--;
+            }
+        }
+        //Debug.Log("Total Player Count " + gm.GetComponent<GameController>().totalPlayerCount);
+        //int activePressurePlate = pressurePlateTotal + 1;
+        //Debug.Log("pressurePlateTotal" + pressurePlateTotal);
+        //int targetbutton = 0;
+        //for (int i = activePressurePlate; i >= gm.GetComponent<GameController>().totalPlayerCount; i--)
+        //{
+        //    Debug.Log("Going to set button to active" + i);
+            
+        //    myButtons[targetbutton].GetComponent<Button>().IShouldBeActive();
+        //    myButtons[targetbutton].GetComponent<Button>().iShouldBeActiveBool = true;
+        //    targetbutton++;
+        //    pressurePlateTotal--;
+        //}
+    }
+
+    public void CheckandIncreaseButtons()
+    {
+        pressurePlatePushedCount++;
+        if (pressurePlatePushedCount > pressurePlateTotal)
+            pressurePlatePushedCount = pressurePlateTotal;
+
+        if (pressurePlatePushedCount == pressurePlateTotal)
+        {
+            foreach (Transform child in transform)
+            {
+                if(child.name == "Door")
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public void CheckandDecreaseButtons()
+    {
+        pressurePlatePushedCount--;
+        if (pressurePlatePushedCount < 0)
+            pressurePlatePushedCount = 0;
+
+            foreach (Transform child in transform)
+            {
+                if (child.name == "Door")
+                {
+                    child.gameObject.SetActive(true);
+                }
+            }
+    }
+}
