2efdfedcf147916579bfc374951b33465586f1cb
Mike Breese <Michael1Breese@gmail.com>
2017-06-29 13:57:28 -0600

    lava spout/jumping hazard and working on water push

---
+++ Assets/Scripts/Level and Puzzles/JumpingHazard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingHazard : MonoBehaviour {
+
+    public GameObject myjumpingHazard;
+
+    public float fireRate;
+
+    public Vector2 shootForce;
+
+    public float destoryHazardsTime;
+
+    private float nextFire;
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Time.time >= nextFire)
+        {
+            SpitJumpingHazard();
+        }
+	}
+
+    void SpitJumpingHazard()
+    {
+        nextFire = Time.time + fireRate;
+        GameObject newJumpingHazard = Instantiate(myjumpingHazard, transform.position, transform.rotation);
+        newJumpingHazard.AddComponent<DestroySelfRightAway>();
+        newJumpingHazard.AddComponent<Rigidbody2D>();
+        newJumpingHazard.GetComponent<DestroySelfRightAway>().waitToDestroyTime = destoryHazardsTime;
+        newJumpingHazard.GetComponent<Rigidbody2D>().AddForce(shootForce, ForceMode2D.Impulse);
+    }
+}
