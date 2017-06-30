f84f671daf7ab530e0297c9f556843352081ad35
Mike Breese <Michael1Breese@gmail.com>
2017-06-29 23:07:29 -0600

    Working on lightning strikes
    particles are hard.

---
+++ Assets/Scripts/Level and Puzzles/LightningStrike.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningStrike : MonoBehaviour {
+
+    public GameObject sparks;
+
+    public Element element;
+
+    public float waitTime = 1;
+
+    // Use this for initialization
+    void Start() {
+        GetComponent<BoxCollider2D>().enabled = false;
+        Invoke("DealDamage", waitTime);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void DealDamage()
+    {//Summon effects
+        //Enable Box Collider2D
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+}
---
+++ Assets/Scripts/Level and Puzzles/ThunderStrikes.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderStrikes : MonoBehaviour {
+
+    public GameObject lightningStrikes;
+
+    public GameObject sparks;
+
+    public float lightningStrikeFireRate;
+
+    private float nextLightningStrike;
+
+    //[SerializeField]
+    private List <GameObject> players;
+	
+	// Update is called once per frame
+	void Update () {
+		//Strike Lightning strike.
+
+        if (Time.time > nextLightningStrike)
+        {
+            LightningStrikes();
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.GetComponent<PlayerHealth>() != null)
+        {
+            players.Add(other.gameObject);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.GetComponent<PlayerHealth>() != null)
+        {
+            players.Remove(other.gameObject);
+        }
+    }
+
+    void LightningStrikes()
+    {
+        foreach (GameObject player in players)
+        {
+            nextLightningStrike = Time.time + lightningStrikeFireRate;
+            if (player.GetComponent<PlayerMovement>().grounded)
+            {
+                Instantiate(sparks, new Vector3(player.transform.position.x, player.transform.position.y - 1, player.transform.position.z), player.transform.rotation);
+                Instantiate(lightningStrikes, player.transform.position, player.transform.rotation);
+            }
+        }
+    }
+}
