89926ae46bc0c81ad1b1f9cb0403f911208fe02a
Mike Breese <Michael1Breese@gmail.com>
2017-07-12 11:17:01 -0600

    character select tool tips
    added tool tips at on the character selection screen.

--- Assets/Scripts/UI and Menu/CharacterSelector.cs
+++ Assets/Scripts/UI and Menu/CharacterSelector.cs
@@ -13,13 +13,35 @@ public class CharacterSelector : MonoBehaviour {
     [HideInInspector]//Makes sure that players can't select the same character as another player.
     public bool alreadySelected = false;
 
+    public GameObject myToolTip;
+
     // Use this for initialization
     void Start () {
-		
+        myToolTip.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+        if (alreadySelected)
+        {
+            myToolTip.SetActive(false);
+        }
 	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (alreadySelected == false)
+        {
+            myToolTip.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (alreadySelected == false)
+        {
+            myToolTip.SetActive(false);
+        }
+    }
 }
