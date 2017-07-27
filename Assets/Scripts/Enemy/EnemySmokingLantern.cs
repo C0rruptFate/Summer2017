9c9acc9ebf18c593e8b244eb1a7e99ad006b4de2
Mike Breese <Michael1Breese@gmail.com>
2017-07-26 22:34:26 -0600

    work on smoking lantern enemy

--- Assets/Scripts/Enemy/EnemySmokingLantern.cs
+++ Assets/Scripts/Enemy/EnemySmokingLantern.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class EnemySmokingLantern : Enemy {
 
+    public float smokeMaxRange;
+    public float smokeTriggerDecrease;
+
+    [HideInInspector]
+    public GameObject smokingLanternSmoke;
+
+    private GameObject enemyWeaponParent;
+
     // Use this for initialization
     protected override void Start()
     {
@@ -11,13 +19,15 @@ public class EnemySmokingLantern : Enemy {
         //animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         element = gameObject.GetComponent<EnemyHealth>().element;
+        smokingLanternSmoke = transform.Find("Smoking Lantern Smoke").gameObject;
 
-        if (useBoundries)
+        enemyWeaponParent = GameObject.Find("Enemy Attacks");
+        if (!enemyWeaponParent)//If it can't find the weapon parent it will create one (the first player on each level should create this automatically).
         {
-            startPosition = transform.position;
+            enemyWeaponParent = new GameObject("Enemy Attacks");
         }
 
-        TargetSelection();//Finds the target.
+        smokingLanternSmoke.transform.parent = enemyWeaponParent.transform;
     }
 
     // Update is called once per frame
