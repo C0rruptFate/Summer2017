e8cb77b25beebcb67f54b5f6d4b11ad17439db8d
Mike Breese <Michael1Breese@gmail.com>
2017-07-30 19:20:49 -0600

    jump out of water effect
    added the jump out of water force.
    
    moved the water SDefend so that the subemitter doesn't show.
    
    Trying to figure out the call Wisp bug.

--- Assets/Scripts/Level and Puzzles/Water.cs
+++ Assets/Scripts/Level and Puzzles/Water.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Water : MonoBehaviour {
+    [Tooltip("The force added to me when I leave the water.")]
+    public float jumpOutOfWaterForce = 3;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -36,11 +38,13 @@ public class Water : MonoBehaviour {
             other.GetComponent<PlayerMovement>().inWater = false;
             other.GetComponent<Rigidbody2D>().mass = other.GetComponent<PlayerMovement>().outOfWaterMass;
             other.GetComponent<PlayerMovement>().arialJumpsUsed = 0;
+            other.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,jumpOutOfWaterForce),ForceMode2D.Impulse);
         }
 
         if (other.tag == "Enemy" && other.GetComponent<EnemyHealth>().element == Element.Ice)//Water enemy is no longer swimming.
         {
             other.GetComponent<Rigidbody2D>().gravityScale = 1;
+            other.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpOutOfWaterForce), ForceMode2D.Impulse);
         }
 
         if (other.GetComponent<Bubble>() != null)
