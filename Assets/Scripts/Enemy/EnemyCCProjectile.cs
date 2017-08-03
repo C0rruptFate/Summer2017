4afef06575054e7364569c56d1ba051070e4ce62
Mike Breese <Michael1Breese@gmail.com>
2017-08-02 22:26:38 -0600

    setting up CC enemy

---
+++ Assets/Scripts/Enemy/EnemyCCProjectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCCProjectile : EnemyProjectile {
+
+    public GameObject target;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void FixedUpdate()
+    {
+        Vector2.MoveTowards(gameObject.transform.position, target.transform.position, projectileSpeed);
+    }
+}
