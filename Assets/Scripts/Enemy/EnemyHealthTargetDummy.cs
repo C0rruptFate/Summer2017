c0c5a9049f5c99b5429d582c6a31c54a2f9fd3db
Mike Breese <Michael1Breese@gmail.com>
2017-08-21 10:53:35 -0600

    work on tutorial level

---
+++ Assets/Scripts/Enemy/EnemyHealthTargetDummy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthTargetDummy : EnemyHealth {
+
+
+    public override void DestroyObject()//Play death animation, wait then enemy dies.
+    {
+        //[TODO]trigger death animation
+
+        Destroy(enemyHPUISliderObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyTargetDummy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTargetDummy : Enemy {
+
+    public override void OnCollisionStay2D(Collision2D other)
+    {
+
+    }
+
+}
